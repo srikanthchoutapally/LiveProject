bb6e7c433d070e3fd5eac4aa8c67ab0e1edda069
Srikanth <Srikanth@DESKTOP-N6360NB>
2019-10-02 11:20:45 +0530

    new package added

---
+++ SrikanthMavenStockAccounting/src/main/java/org/core/java/Test1.java
@@ -0,0 +1,12 @@
+package org.core.java;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+
+	@Test
+	public void t1()
+	{
+		System.err.print("hello how r u?");
+	}
+}
---
+++ SrikanthMavenStockAccounting/src/main/java/org/core/java/Test2.java
@@ -0,0 +1,13 @@
+package org.core.java;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void t1()
+	{
+		System.err.print("t1  ?");
+	}
+
+}
